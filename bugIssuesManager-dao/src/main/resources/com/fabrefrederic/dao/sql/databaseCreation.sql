8bfb65360a8d146ea21840b74e06bb16a4c9b770
Frederic Fabre <fabrefrederic@free.fr>
2015-01-16 13:59:59 +0100

    Sql development : sequence and tables sql creation

---
+++ bugIssuesManager-dao/src/main/resources/com/fabrefrederic/dao/sql/databaseCreation.sql
@@ -0,0 +1,9 @@
+-- Sequence: bugIssuesManager_id_seq
+ CREATE SEQUENCE bugIssuesManager_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  START 1
+  CACHE 1;
+
+ALTER TABLE bugIssuesManager_id_seq
+ OWNER TO fred;
---
+++ bugIssuesManager-dao/src/main/resources/com/fabrefrederic/dao/sql/tableCreation.sql
@@ -0,0 +1,35 @@
+create table file (
+    file_id int8 not null,
+    file_name varchar(255),
+    file_path varchar(255),
+    primary key (file_id)
+);
+
+ALTER TABLE file
+ALTER COLUMN file_id
+SET DEFAULT nextval('bugIssuesManager_id_seq');
+
+create table commit (
+    commit_id int8 not null,
+    commit_number varchar(255),
+    commit_date timestamp with time zone,
+    commit_developer varchar(80),
+    commit_issue int8,
+    commit_file int8 not null,
+    primary key (commit_id)
+);
+
+ALTER TABLE commit
+ALTER COLUMN commit_id
+SET DEFAULT nextval('bugIssuesManager_id_seq');
+
+create table issue (
+    issue_id int8 not null,
+    issue_name varchar(10),
+    issue_description varchar(255),
+    primary key (issue_id)
+);
+
+ALTER TABLE issue
+ALTER COLUMN issue_id
+SET DEFAULT nextval('bugIssuesManager_id_seq');
