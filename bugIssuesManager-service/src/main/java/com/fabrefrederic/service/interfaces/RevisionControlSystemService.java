edb9ce4a1eb761ce2395ba9682945b01e905b78c
Frederic Fabre <fabrefrederic@free.fr>
2015-01-20 14:59:56 +0100

    Library implementation and use - in progress

--- bugIssuesManager-service/src/main/java/com/fabrefrederic/service/interfaces/RevisionControlSystemService.java
+++ bugIssuesManager-service/src/main/java/com/fabrefrederic/service/interfaces/RevisionControlSystemService.java
@@ -1,9 +1,24 @@
 package com.fabrefrederic.service.interfaces;
 
+import java.util.Collection;
 import java.util.List;
+
+import org.tmatesoft.svn.core.SVNLogEntry;
+
+import com.fabrefrederic.business.Commit;
 
 public interface RevisionControlSystemService {
 
-    List<Commit> getCommitInformation();
+    /**
+     *
+     * @return
+     */
+    Collection<SVNLogEntry> getCommits(long startRevision, long endRevision);
+
+    /**
+     *
+     * @param commits
+     */
+    void saveCommits(List<Commit> commits);
 
 }
