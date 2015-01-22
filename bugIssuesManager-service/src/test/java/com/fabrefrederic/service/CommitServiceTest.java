2205f4d395815dab5c0a74c597db2bb95276518d
Fred <fabrefrederic@free.fr>
2015-01-22 23:00:34 +0100

    test in progress

--- bugIssuesManager-service/src/test/java/com/fabrefrederic/service/CommitServiceTest.java
+++ bugIssuesManager-service/src/test/java/com/fabrefrederic/service/CommitServiceTest.java
@@ -5,10 +5,12 @@ package com.fabrefrederic.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"service/src/test/resources/com/fabrefrederic/test/service/spring/applicationContext-service.xml"})
-@ContextConfiguration(locations = {"classpath:applicationContext-dao-test.xml"})
+@ContextConfiguration(locations={"service/src/test/resources/com/fabrefrederic/service/spring/applicationContext-service.xml", 
+		"classpath:applicationContext-dao.xml", "classpath:applicationContext-library.xml"})
 public class CommitServiceTest  {
 
     @Test
