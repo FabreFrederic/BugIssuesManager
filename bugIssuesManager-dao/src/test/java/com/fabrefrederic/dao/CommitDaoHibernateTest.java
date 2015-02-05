433757f387d9a88af7494301e099311ece3252f9
Frederic Fabre <fabrefrederic@free.fr>
2015-02-05 14:01:48 +0100

    DAO - test findByCommitNumber - in progress

--- bugIssuesManager-dao/src/test/java/com/fabrefrederic/dao/CommitDaoHibernateTest.java
+++ bugIssuesManager-dao/src/test/java/com/fabrefrederic/dao/CommitDaoHibernateTest.java
@@ -12,14 +12,14 @@ import com.fabrefrederic.dao.interfaces.CommitDao;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:/com/fabrefrederic/dao/spring/applicationContext-dao-test.xml" })
 public class CommitDaoHibernateTest {
-	@Autowired
-	CommitDao commitDao;
+    @Autowired
+    CommitDao commitDao;
 
-	@Test
-	public void test() {
-		final Commit commit = commitDao.findByCommitNumber("1");
-		if (commit != null) {
-			System.out.println("toto");
-		}
-	}
+    @Test
+    public void test() {
+        final Commit commit = commitDao.findByCommitNumber("1");
+        if (commit != null) {
+            System.out.println("toto");
+        }
+    }
 }
