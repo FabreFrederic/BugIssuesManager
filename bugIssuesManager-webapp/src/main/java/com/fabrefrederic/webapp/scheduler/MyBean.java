3da7b9725a9851484651d83fadfc276c7a4671af
Frederic Fabre <fabrefrederic@free.fr>
2015-02-24 16:02:44 +0100

    web app scheduler

--- bugIssuesManager-webapp/src/main/java/com/fabrefrederic/webapp/scheduler/MyBean.java
+++ bugIssuesManager-webapp/src/main/java/com/fabrefrederic/webapp/scheduler/MyBean.java
@@ -6,9 +6,9 @@ import org.springframework.scheduling.annotation.Scheduled;
 public class MyBean {
     private static final Logger LOGGER = Logger.getLogger(MyBean.class);
 
-    @Scheduled(fixedRate=5000)
+    @Scheduled(fixedRate=1000)
     public void printMessage() {
         System.out.println("I am called by Spring scheduler - system");
-        LOGGER.info("I am called by Spring scheduler");
+        LOGGER.debug("I am called by Spring scheduler");
     }
 }
--- bugIssuesManager-webapp/src/main/java/com/fabrefrederic/webapp/scheduler/SchedulerConfiguration.java
+++ bugIssuesManager-webapp/src/main/java/com/fabrefrederic/webapp/scheduler/SchedulerConfiguration.java
@@ -1,15 +1,19 @@
 package com.fabrefrederic.webapp.scheduler;
 
-import org.springframework.context.annotation.Bean;
+import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 
 @Configuration
 @EnableScheduling
 public class SchedulerConfiguration {
-    @Bean
-    public MyBean bean() {
-        return new MyBean();
+    private static final Logger LOGGER = Logger.getLogger(SchedulerConfiguration.class);
+
+    @Scheduled(fixedRate=1000)
+    public void printMessage() {
+        System.out.println("I am called by Spring scheduler - system");
+        LOGGER.debug("I am called by Spring scheduler");
     }
 }
