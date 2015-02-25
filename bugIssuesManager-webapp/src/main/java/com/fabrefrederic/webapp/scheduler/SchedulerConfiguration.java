cc5fac951952acd54f86c2a265292fcf429fdeca
Frederic Fabre <fabrefrederic@free.fr>
2015-02-25 17:30:23 +0100

    scheduler ok in console

--- bugIssuesManager-webapp/src/main/java/com/fabrefrederic/webapp/scheduler/SchedulerConfiguration.java
+++ bugIssuesManager-webapp/src/main/java/com/fabrefrederic/webapp/scheduler/SchedulerConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 public class SchedulerConfiguration {
     private static final Logger LOGGER = Logger.getLogger(SchedulerConfiguration.class);
 
-    @Scheduled(fixedRate=1000)
+    @Scheduled(cron = "${scheduler.cron}")
     public void printMessage() {
         System.out.println("I am called by Spring scheduler - system");
         LOGGER.debug("I am called by Spring scheduler");
