2a004469ec3c33f36fee8655b36b3df0198c4460
Fred <fabrefrederic@free.fr>
2015-04-25 23:05:43 +0200

    scheduler to retrive the commits - in progress

--- bugIssuesManager-service/src/main/java/com/fabrefrederic/service/CommitServiceImpl.java
+++ bugIssuesManager-service/src/main/java/com/fabrefrederic/service/CommitServiceImpl.java
@@ -12,61 +12,63 @@ import com.fabrefrederic.library.RevisionControlSystem;
 import com.fabrefrederic.service.interfaces.CommitService;
 
 public class CommitServiceImpl implements CommitService {
-    private static final Logger LOGGER = Logger.getLogger(CommitService.class);
+	private static final Logger LOGGER = Logger.getLogger(CommitService.class);
 
-    @Autowired
-    private RevisionControlSystem revisionControlSystem;
-    @Autowired
-    private CommitDao commitDao;
+	@Autowired
+	private RevisionControlSystem revisionControlSystem;
+	@Autowired
+	private CommitDao commitDao;
 
-    @Override
-    public List<Commit> getCommits(final String path, final long startRevision, final long endRevision) {
-        List<Commit> commits = new ArrayList<Commit>();
-        try {
-            commits = revisionControlSystem.getLogs(path, startRevision, endRevision);
-        } catch (final Exception e) {
-            LOGGER.error(e);
-        }
-        LOGGER.debug("Number of commits retrieved : " + commits.size());
-        return commits;
-    }
+	@Override
+	public List<Commit> getCommits(final String path, final long startRevision, final long endRevision) {
+		List<Commit> commits = new ArrayList<Commit>();
+		try {
+			commits = revisionControlSystem.getLogs(path, startRevision, endRevision);
+		} catch (final Exception e) {
+			LOGGER.error(e);
+		}
+		LOGGER.debug("Number of commits retrieved : " + commits.size());
+		return commits;
+	}
 
-    @Override
-    public void saveCommits(final List<Commit> commits) {
-        for (final Commit commit : commits) {
-            commitDao.save(commit);
-        }
-    }
+	@Override
+	public void saveCommits(final List<Commit> commits) {
+		if (commits != null) {
+			for (final Commit commit : commits) {
+				commitDao.save(commit);
+			}
+		}
+	}
 
-    @Override
-    public List<Commit> getCommitsToTheLastRevision(String path, long startRevision) {
-        List<Commit> commits = new ArrayList<Commit>();
-        try {
-            commits = revisionControlSystem.getLogsToTheLastRevision(path, startRevision);
-        } catch (final Exception e) {
-            LOGGER.error(e);
-        }
-        LOGGER.debug("Number of commits retrieved : " + commits.size());
-        return commits;
-    }
+	@Override
+	public Commit getTheLastSavedCommit() {
+		final Commit commit = commitDao.findTheMostRecentCommit();
+		return commit;
+	}
 
-    @Override
-    public Commit getTheLastSavedCommit() {
-        final Commit commit = commitDao.findTheMostRecentCommit();
-        return commit;
-    }
+	@Override
+	public List<Commit> getCommitsToTheLastRevision(String path, long startRevision) {
+		List<Commit> commits = new ArrayList<Commit>();
+		try {
+			commits = revisionControlSystem.getLogsToTheLastRevision(path, startRevision);
+		} catch (final Exception e) {
+			LOGGER.error(e);
+		}
+		LOGGER.debug("Number of commits retrieved : " + commits.size());
+		return commits;
+	}
 
-    /**
-     * @param revisionControlSystem the revisionControlSystem to set
-     */
-    public void setRevisionControlSystem(RevisionControlSystem revisionControlSystem) {
-        this.revisionControlSystem = revisionControlSystem;
-    }
+	/**
+	 * @param revisionControlSystem the revisionControlSystem to set
+	 */
+	public void setRevisionControlSystem(RevisionControlSystem revisionControlSystem) {
+		this.revisionControlSystem = revisionControlSystem;
+	}
 
-    /**
-     * @param commitDao the commitDao to set
-     */
-    public void setCommitDao(CommitDao commitDao) {
-        this.commitDao = commitDao;
-    }
+	/**
+	 * @param commitDao the commitDao to set
+	 */
+	public void setCommitDao(CommitDao commitDao) {
+		this.commitDao = commitDao;
+	}
 }
--- bugIssuesManager-webapp/src/main/java/com/fabrefrederic/webapp/scheduler/SchedulerConfiguration.java
+++ bugIssuesManager-webapp/src/main/java/com/fabrefrederic/webapp/scheduler/SchedulerConfiguration.java
@@ -1,19 +1,56 @@
 package com.fabrefrederic.webapp.scheduler;
 
+import java.util.Date;
+import java.util.List;
+
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import com.fabrefrederic.business.Commit;
+import com.fabrefrederic.service.interfaces.CommitService;
 
 @Configuration
 @EnableScheduling
 public class SchedulerConfiguration {
-    private static final Logger LOGGER = Logger.getLogger(SchedulerConfiguration.class);
+	private static final Logger LOGGER = Logger.getLogger(SchedulerConfiguration.class);
 
-    @Scheduled(cron = "${scheduler.cron}")
-    public void printMessage() {
-        System.out.println("I am called by Spring scheduler - system");
-        LOGGER.debug("I am called by Spring scheduler");
-    }
+	@Autowired
+	private CommitService commitService;
+
+	@Value("${repository.path}")
+	String repositoryPath;
+
+	@Scheduled(cron = "${scheduler.cron}")
+	public void printMessage() {
+		System.out.println("I am called by Spring scheduler - system");
+		LOGGER.debug("I am called by Spring scheduler");
+	}
+
+	@Scheduled(cron = "${scheduler.cron}")
+	public void saveNewCommitsFromRepository() {
+		// TODO : delete all the system.out
+		System.out.println("saveNewCommitsFromRepository() started : " + new Date());
+		LOGGER.debug("saveNewCommitsFromRepository() started : " + new Date());
+
+		final Commit lastCommit = commitService.getTheLastSavedCommit();
+		if (lastCommit != null) {
+			System.out.println("lastCommit.getNumber() : " + lastCommit.getNumber());
+			System.out.println("lastCommit.getDate() : " + lastCommit.getDate());
+			LOGGER.debug("lastCommit.getNumber() : " + lastCommit.getNumber());
+			LOGGER.debug("lastCommit.getDate() : " + lastCommit.getDate());
+			final List<Commit> commits = commitService.getCommitsToTheLastRevision(repositoryPath, Long.valueOf(lastCommit.getNumber()));
+			if (commits != null && commits.size() > 0) {
+				commits.remove(lastCommit);
+				commitService.saveCommits(commits);
+			}
+		} else {
+			System.out.println("No commit found in datebase, we have to retrieve the commits in database before launching the cron");
+			LOGGER.debug("No commit found in datebase, we have to retrieve the commits in database before launching the cron");
+		}
+
+	}
 }
