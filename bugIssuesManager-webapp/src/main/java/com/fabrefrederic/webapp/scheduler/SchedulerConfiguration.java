dd32d469ac776108cc8a1d064007ff56a27145a6
Frederic Fabre <fabrefrederic@free.fr>
2015-05-15 17:22:39 +0200

    Scheduler

--- bugIssuesManager-webapp/src/main/java/com/fabrefrederic/webapp/scheduler/SchedulerConfiguration.java
+++ bugIssuesManager-webapp/src/main/java/com/fabrefrederic/webapp/scheduler/SchedulerConfiguration.java
@@ -3,6 +3,7 @@ package com.fabrefrederic.webapp.scheduler;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -34,46 +35,51 @@ public class SchedulerConfiguration {
 
     @Scheduled(cron = "${scheduler.cron}")
     public void saveNewCommitsFromRepository() {
-        // Default value
-        if (searchLimit == null) {
-            searchLimit = "10";
-        }
-        final Integer limit = Integer.valueOf(searchLimit);
-
-        // TODO : develop a method to know the running time
         LOGGER.debug("saveNewCommitsFromRepository() started : " + new Date());
+
+        final Integer limit = getSearchLimit();
 
         Commit firstCommit = commitService.getTheLastSavedCommit();
         if (firstCommit == null) {
+            LOGGER.debug("The first commit has not been find in DB");
             firstCommit = commitService.getTheFirstCommitFromRepository(repositoryPath);
         }
-        final List<Commit> commits = commitService.getCommitsToTheLastRevision(repositoryPath,
-                firstCommit.getNumber(), limit);
-        if (commits != null && commits.size() > 0) {
-            for (final Commit commit : commits) {
-                final Issue issue = issueService.getIssuesFromMessage(commit.getMessage());
-                commit.setIssue(issue);
+
+        if (firstCommit != null) {
+            LOGGER.debug("First commit number from DB : " + firstCommit.getNumber());
+            final List<Commit> commits = commitService.getCommitsToTheLastRevision(repositoryPath,
+                    firstCommit.getNumber(), limit);
+            if (commits != null && commits.size() > 0) {
+                for (final Commit commit : commits) {
+                    final Issue issue = issueService.getIssuesFromMessage(commit.getMessage());
+                    commit.setIssue(issue);
+                }
+                LOGGER.debug("Number of commits to save from the repository : " + commits.size());
+                commitService.saveCommits(commits);
+            } else {
+                LOGGER.info("No new commits from the repository");
             }
-            commitService.saveCommits(commits);
+        } else {
+            LOGGER.warn("The first commit has not been find from the repository");
         }
-        else {
-            LOGGER.info("No commit found");
-        }
-
     }
 
     /**
-     * @param commitService the commitService to set
+     * @return the search limit
      */
-    public void setCommitService(CommitService commitService) {
-        this.commitService = commitService;
-    }
-
-    /**
-     * @param issueService the issueService to set
-     */
-    public void setIssueService(IssueService issueService) {
-        this.issueService = issueService;
+    private Integer getSearchLimit() {
+        Integer limit = null;
+        if (StringUtils.isNotBlank(searchLimit)) {
+            try {
+                limit = Integer.valueOf(searchLimit);
+            } catch (final NumberFormatException exception) {
+                LOGGER.warn("Search limit does not have the appropriate format or is not is not a number", exception);
+            }
+        } else {
+            limit = 10;
+            LOGGER.warn("Search limit is not configured. default value is 10");
+        }
+        return limit;
     }
 
 }
